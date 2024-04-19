78ca8674f0d5e8a1ecec3b4f57815c2d7da95f1f
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2024-04-19 06:10:30 +0530

    get-adding-arrays

---
+++ 07-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIE=("RRR" "DJTILLU" "KGF")
+
+#size of above array is 3.
+#index are 0,1,2
+#list are always starts from 0,1,2
+
+echo "FIRST Movie is: ${MOVIE[o]}"
+echo "SECOND Movie is: ${MOVIE[1]}"
+echo "LIST OF All Movie: ${MOVIE[@]}"
