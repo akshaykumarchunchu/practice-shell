9723137db729c710c09313fb357726f09e7b6e87
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2024-04-19 06:14:18 +0530

    first-message

---
+++ 08-speical-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+$@: is the symbol of all variables to print all variables we should give 01-variables.sh
+$#: is the number of variables we passed
+$0: to know the script name
+PWD: Caps is the present working directory
+$HOEM: home directory of current directory
+$USER: which user is running script
+"process id of the current shell script: $$"
+$!: TO check the process ID of last background command
+id -u: is normal user
+id 0: is root user
