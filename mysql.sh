6f675cae9e828ee9dff9804c1eabf9e1f0fe92b5
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2024-04-23 03:23:52 +0530

    first-message

--- mysql.sh
+++ mysql.sh
@@ -37,5 +37,15 @@ VALIDATE $? "Enabling mysql server"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting Mysql server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting up root password"
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#VALIDATE $? "Setting up root password"
+
+#below code will be useful for idemoptent nature 
+mysql -h akshaydaws-78s.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
