b968b6df5bb32abf33b48bff6bfce030400f646e
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2024-04-23 00:58:16 +0530

    first-message

---
+++ 14-install-packages.sh
@@ -0,0 +1,15 @@
+#!\bin\bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then 
+	echo "Install the script with root user"
+	exit 1                          
+else
+	echo "Your a super user"
+fi
+
+for i $0
+do 
+echo "package install $1"
+done
