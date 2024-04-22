c26e9f6872736597ab6aaf2bfb422e998dc0c327
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2024-04-23 02:46:39 +0530

    first-message

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -5,6 +5,11 @@ TIMESTAMP=$(date +%F-%H-%M-%S) #to know the date,time,month,hours,secs of creati
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #--> To cut the last name of script name after . and take first name
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0"
+
 VALIDATE(){                 #function syntax
 if [ $1 -ne 0 ]             #$1 means variable 1 that is in "dnf install" VALIDATE $?
 then 	
