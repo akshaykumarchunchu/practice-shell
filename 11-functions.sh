9b9a7230a8403cbcbc7368ba03593484bfee3f81
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2024-04-23 00:01:36 +0530

    first-message

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,19 +5,20 @@ TIME_STAMP=$(date +%F-%H-%M-%S) #to know the date,time,month,hours,secs of creat
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #--> To cut the last name of script name after . and take first name
 LOGFILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log #---> to know where the tmp data is saved 
 
-VALIDATE(){  #function syntax
-if [ $1 -ne 0 ]         #$1 means variable 1 that is in "dnf install" VALIDATE $?
+VALIDATE(){                 #function syntax
+if [ $1 -ne 0 ]             #$1 means variable 1 that is in "dnf install" VALIDATE $?
 then 	
-    echo "$2 --- failure" #$2 means variable 1 that is in "dnf install" "Install mysql"
-    exit
+    echo "$2 --- failure"   #$2 means variable 1 that is in "dnf install" "Install mysql"
+    exit 1
 else
 	echo "$2 --- Success"
 fi
+}
 
 if [ $USERID -ne 0 ]
 then 
 	echo "Install the script with root user"
-	exit 1
+	exit 1                          #manually exit if error comes.
 else
 	echo "Your a super user"
 f1
