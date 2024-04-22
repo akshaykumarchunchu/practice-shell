4d1757fd5f09aaaadbeb3a7072b05cf5a5aecbc8
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2024-04-23 01:18:21 +0530

    first-message

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,20 @@
 #!\bin\bash
 
-USERID=$(id -u)
+USERID=$(id -u) #to know the user id number to install on root user
+TIMESTAMP=$(date +%F-%H-%M-%S) #to know the date,time,month,hours,secs of creating the logfile
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #--> To cut the last name of script name after . and take first name
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP
+
+VALIDATE(){                 #function syntax
+if [ $1 -ne 0 ]             #$1 means variable 1 that is in "dnf install" VALIDATE $?
+then 	
+    echo "$2 --- failure"   #$2 means variable 1 that is in "dnf install" "Install mysql"
+    exit 1
+else
+	echo "$2 --- Success"
+fi
+}
+
 if [ $USERID -ne 0 ]
 then 
 	echo "Install the script with root user"
@@ -11,5 +25,12 @@ fi
 
 for i in $@
 do 
-echo "package install $i"
+echo "package install: $i"
+dnf list installed $i &>>LOGFILE
+if [ $? -eq 0 ]
+then 
+    echo "$i already installed...Skipping"
+else
+    echo "$i not installed..Need to instal"
+fi     
 done
