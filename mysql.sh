9dc46b95b0ca67a0e46b97ea51ff62c6d68337a0
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2024-04-23 18:07:22 +0530

    first-message

--- mysql.sh
+++ mysql.sh
@@ -9,7 +9,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0"
-
+echo "Please enter DB Password"
+read -s mysql_root_password
 
 VALIDATE(){                                 #function syntax
 if [ $1 -ne 0 ]                             #$1 means variable 1 that is in "dnf install" VALIDATE $? Arguments
@@ -42,10 +43,10 @@ VALIDATE $? "Starting Mysql server"
 #VALIDATE $? "Setting up root password"
 
 #below code will be useful for idemoptent nature 
-mysql -h database.akshaydaws-78s.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h database.akshaydaws-78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
